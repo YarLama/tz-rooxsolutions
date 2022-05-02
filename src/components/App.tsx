7eba4f0e30694dd385d9a128bdef201b687e0dc4
YarLama <gaideichuk1998@mail.ru>
2022-05-02 12:54:23 +0300

    update: UserInfoPage add form fields

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,9 +5,34 @@ import '../styles/_typography.scss'
 import cl from './app.scss'
 import SortList from './SortList/SortList';
 import UserPage from '../pages/UsersPage/UserPage';
+import UserInfoPage from '../pages/UserInfoPage/UserInfoPage';
 
 
 const App: React.FC = () => {
+
+    const user = {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+          "street": "Kulas Light",
+          "suite": "Apt. 556",
+          "city": "Gwenborough",
+          "zipcode": "92998-3874",
+          "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+          }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+          "name": "Romaguera-Crona",
+          "catchPhrase": "Multi-layered client-server neural-net",
+          "bs": "harness real-time e-markets"
+        }
+      }
 
     return (
         <div className={cl.app}>
@@ -15,7 +40,8 @@ const App: React.FC = () => {
                 <SortList />
             </div>
             <div className={cl.content_block}>
-                <UserPage />
+                {/* <UserPage /> */}
+                <UserInfoPage user={user}/>
             </div>
             
         </div>
--- src/pages/UserInfoPage/UserInfoPage.tsx
+++ src/pages/UserInfoPage/UserInfoPage.tsx
@@ -1,11 +1,71 @@
+import { Formik, Form, Field } from 'formik';
 import React from 'react';
+import {IUser} from '../../types/types'
 
+interface IFormValues {
+    name : string;
+    username : string;
+    email: string;
+    street: string;
+    city: string;
+    zip: string;
+    phone: string;
+    website: string;
+}
 
-const UserInfoPage: React.FC = () => {
+interface UserInfoPageProps {
+    user: IUser,
+}
+
+const UserInfoPage: React.FC<UserInfoPageProps> = ({user}) => {
+    const initialValues: IFormValues = {
+        name : '',
+        username : '',
+        email: '',
+        street: '',
+        city: '',
+        zip: '',
+        phone: '',
+        website: '',
+    };
     
     return (
         <div>
             <h1>Профиль пользователя</h1>
+            <Formik
+                initialValues={initialValues}
+                onSubmit={(values, actions) => {
+                    console.log(values, actions)
+                }}
+            >
+                <Form>
+                    <label htmlFor="name">Name</label>
+                    <Field name="name"/>
+
+                    <label htmlFor="username">User name</label>
+                    <Field name="username"/>
+
+                    <label htmlFor="email">E-mail</label>
+                    <Field name="email"/>
+
+                    <label htmlFor="street">Street</label>
+                    <Field name="street"/>
+
+                    <label htmlFor="city">City</label>
+                    <Field name="city"/>
+
+                    <label htmlFor="zip">Zip code</label>
+                    <Field name="zip"/>
+
+                    <label htmlFor="phone">Phone</label>
+                    <Field name="phone"/>
+
+                    <label htmlFor="website">Website</label>
+                    <Field name="website"/>
+
+                    <button>Отправить</button>
+                </Form>
+            </Formik>
         </div>
         
     );
