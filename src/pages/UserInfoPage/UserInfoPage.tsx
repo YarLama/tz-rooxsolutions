7efd9e76040b2f7bbda56a0fc896bb5bd525efea
YarLama <gaideichuk1998@mail.ru>
2022-05-03 22:36:32 +0300

    update: add some form fields in userinfopage and some functions

--- src/pages/UserInfoPage/UserInfoPage.tsx
+++ src/pages/UserInfoPage/UserInfoPage.tsx
@@ -1,6 +1,7 @@
-import { Formik, Form, Field } from 'formik';
-import React from 'react';
-import {IUser} from '../../types/types'
+import { Formik, Form, Field, FormikHelpers, useField, useFormik } from 'formik';
+import React, { useState } from 'react';
+import {IUser} from '../../types/types';
+import cl from './userInfoPage.scss'
 
 interface IFormValues {
     name : string;
@@ -11,61 +12,142 @@ interface IFormValues {
     zip: string;
     phone: string;
     website: string;
+    comment?: string;
 }
 
 interface UserInfoPageProps {
     user: IUser,
+    comment?: string;
 }
 
+
 const UserInfoPage: React.FC<UserInfoPageProps> = ({user}) => {
+
+    const [isFieldsDisabled, setIsFieldDisabled] = useState<boolean>(true)
+
     const initialValues: IFormValues = {
-        name : '',
-        username : '',
-        email: '',
-        street: '',
-        city: '',
-        zip: '',
-        phone: '',
-        website: '',
+        name : user.name,
+        username : user.username,
+        email: user.email,
+        street: user.address.street,
+        city: user.address.city,
+        zip: user.address.zipcode,
+        phone: user.phone,
+        website: user.website,
+        comment: ""
     };
+
+    const submitData = (
+        values: IFormValues, 
+        actions: FormikHelpers<IFormValues>): void => {
+        console.log(values, actions)
+    }
+
+    const formik = useFormik({
+        initialValues: initialValues,
+        onSubmit : submitData
+    })
     
     return (
         <div>
-            <h1>Профиль пользователя</h1>
-            <Formik
-                initialValues={initialValues}
-                onSubmit={(values, actions) => {
-                    console.log(values, actions)
-                }}
-            >
-                <Form>
+            <div className={cl.user_label_block}>
+                <h1>Профиль пользователя</h1>
+                <button onClick={() => {setIsFieldDisabled(!isFieldsDisabled)}}>Редактировать</button>
+            </div>
+            <form onSubmit={formik.handleSubmit} className={cl.user_form}>
+                <div className={cl.user_form_fields}>
                     <label htmlFor="name">Name</label>
-                    <Field name="name"/>
+                    <input
+                    type="text"
+                    name="name" 
+                    placeholder="name" 
+                    value={formik.values.name}
+                    onChange={formik.handleChange}
+                    disabled = {isFieldsDisabled}
+                    />
 
                     <label htmlFor="username">User name</label>
-                    <Field name="username"/>
+                    <input
+                    type="text"
+                    name="username" 
+                    placeholder="username" 
+                    value={formik.values.username}
+                    onChange={formik.handleChange}
+                    disabled = {isFieldsDisabled}
+                    />
 
                     <label htmlFor="email">E-mail</label>
-                    <Field name="email"/>
+                    <input
+                    type="text"
+                    name="email" 
+                    placeholder="email" 
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    disabled = {isFieldsDisabled}
+                    />
 
                     <label htmlFor="street">Street</label>
-                    <Field name="street"/>
+                    <input
+                    type="text"
+                    name="street" 
+                    placeholder="street" 
+                    value={formik.values.street}
+                    onChange={formik.handleChange}
+                    disabled = {isFieldsDisabled}
+                    />
 
                     <label htmlFor="city">City</label>
-                    <Field name="city"/>
+                    <input
+                    type="text"
+                    name="city" 
+                    placeholder="city" 
+                    value={formik.values.city}
+                    onChange={formik.handleChange}
+                    disabled = {isFieldsDisabled}
+                    />
 
                     <label htmlFor="zip">Zip code</label>
-                    <Field name="zip"/>
+                    <input
+                    type="text" 
+                    name="zip" 
+                    placeholder="zip" 
+                    value={formik.values.zip}
+                    onChange={formik.handleChange}
+                    disabled = {isFieldsDisabled}
+                    />
 
                     <label htmlFor="phone">Phone</label>
-                    <Field name="phone"/>
+                    <input
+                    type="text" 
+                    name="phone" 
+                    placeholder="phone" 
+                    value={formik.values.phone}
+                    onChange={formik.handleChange}
+                    disabled = {isFieldsDisabled}
+                    />
 
                     <label htmlFor="website">Website</label>
-                    <Field name="website"/>
-
-                    <button>Отправить</button>
-                </Form>
-            </Formik>
+                    <input
+                    type="text" 
+                    name="website" 
+                    placeholder="website" 
+                    value={formik.values.website}
+                    onChange={formik.handleChange}
+                    disabled = {isFieldsDisabled}
+                    />
+                    
+                    <label htmlFor="comment">Comment</label>
+                    <textarea 
+                        name="comment"
+                        value={formik.values.comment}
+                        onChange={formik.handleChange}
+                        disabled = {isFieldsDisabled}
+                    />
+                </div>
+                <div className={cl.user_form_tooltip}>
+                    <button type='submit' disabled={isFieldsDisabled}>Отправить</button>
+                </div>
+            </form>
         </div>
         
     );
